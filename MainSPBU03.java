95ce619e9088e65d696dae544ec8f8196a0fe165
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-06-04 15:11:26 +0700

    add Class MainSPBU03 First

---
+++ MainSPBU03.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class MainSPBU03 {
+    public static void main(String[] args) {
+        Scanner sc03 = new Scanner(System.in);
+        int pilihan;
+        do {
+            System.out.println("\n============= MENU SPBU  ============");
+            System.out.println("1. Tambah Antrian");
+            System.out.println("2. Tampilkan Antrian");
+            System.out.println("3. Check Jumlah Antrian");
+            System.out.println("4. Layani Kendaraan");
+            System.out.println("5. Tampilkan Riwayat Transaksi");
+            System.out.println("0. Keluar");
+            System.out.println("======================================");
+            System.out.print("Masukkan Pilihan Anda: ");
+            pilihan = sc03.nextInt();
+            sc03.nextLine();
+            System.out.println();
+            switch (pilihan){
+                case 1 -> {
+                    
+                }
+                case 2 -> {
+                    
+                }
+                case 3 -> {
+                    
+                }
+                case 4 -> {
+                   
+                }
+                case 5 -> {
+                   
+                }
+                case 0 -> {
+                    
+                }
+                default -> {
+                    System.out.println("Pilihan tidak valid, silakan coba lagi.");
+                }
+            }
+        } while (pilihan != 0);
+        System.out.println();
+    }
+}
