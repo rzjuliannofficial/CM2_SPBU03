8955804df1ff213942294d89716ed29e5c59c6d2
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-06-06 19:31:06 +0700

    add Class Node03

---
+++ Node03.java
@@ -0,0 +1,9 @@
+public class Node03 {
+    Kendaraan03 kendaraan;
+    Node03 next;
+
+    public Node03(Kendaraan03 kendaraan , Node03 next) {
+        this.kendaraan = kendaraan;
+        this.next = null;
+    }    
+}
